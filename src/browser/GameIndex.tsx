385cc3d4c2b83d81818b4b3a1befb3c266532125
Jussi Mononen <jussi.mononen@nitor.com>
2021-02-09 21:55:14 +0200

    Add spinner for loading

--- src/browser/GameIndex.tsx
+++ src/browser/GameIndex.tsx
@@ -3,11 +3,13 @@ import Game from "./Game";
 import Api from "./Api";
 import { gainsboro } from "color-name";
 import { GameTO } from "../server/types";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-class GameIndex extends React.Component<{}, {games: GameTO[]}> {
+class GameIndex extends React.Component<{}, {games: GameTO[], loading: boolean}> {
     constructor(props: any) { 
         super(props);
-        this.state = { games: [] };
+        this.state = { games: [], loading: true };
     }
     async componentDidMount() {
         try {
@@ -17,29 +19,36 @@ class GameIndex extends React.Component<{}, {games: GameTO[]}> {
             }
             const json = await response.json();
             console.log('games', json)
-            this.setState({ games: json });
+            this.setState({ games: json, loading: false });
         } catch (error) {
             console.log('Failed to fetch game information', error)
+            this.setState({ loading: false });
         }
     }
 
     render() {
+
+        const spinner = <Loader type="Puff" color="#C70039" height={80} width={80}/>;
+
         const games = this.state.games.map((g: GameTO) => <Game game={g} key={g.timestamp}/>);
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Sarja</th>
-                        <th>Kotijoukkue</th>
-                        <th>Vierasjoukkue</th>
-                        <th>Pelipäivä</th>
-                        <th>Linkki pelin lisätietoihin</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {games}
-                </tbody>
-            </table>
+            <div>
+                {this.state.loading ? spinner :
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Sarja</th>
+                            <th>Kotijoukkue</th>
+                            <th>Vierasjoukkue</th>
+                            <th>Pelipäivä</th>
+                            <th>Linkki pelin lisätietoihin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {games}
+                    </tbody>
+                </table>}
+            </div>
         );
     };
 }
