8fd27287d61915f108557139804ce79cf23270a6
Jussi Mononen <jussi.mononen@nitor.com>
2021-02-09 15:10:50 +0200

    App loads now

--- src/browser/GameIndex.tsx
+++ src/browser/GameIndex.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import Game from "./Game";
 import Api from "./Api";
 import { gainsboro } from "color-name";
+import { GameTO } from "../server/types";
 
-class GameIndex extends React.Component {
+class GameIndex extends React.Component<{}, {games: GameTO[]}> {
     constructor(props: any) { 
         super(props);
         this.state = { games: [] };
     }
-    componentDidMount() {
-        Api.fetchGames()
-            .then(res => res.json())
-            .then(games => {
-                this.setState({ games: games });
-            });
+    async componentDidMount() {
+        const response = await Api.fetchGames();
+        const json = await response.json();
+        this.setState({ games: this.mapJsonToGames(json) });
     }
+
+    mapJsonToGames = (json: string) => {
+        const games: GameTO[] = JSON.parse(json);
+        console.log('gameTO\'s', games)
+        return games;
+    };
 
     render() {
         return (
             <div>
-                <Game game={true} />
+                {this.state.games.map((g: any) => (<Game game={g} />))}
             </div>
         );
     };
